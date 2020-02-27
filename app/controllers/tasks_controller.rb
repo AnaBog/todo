022ec33e8a3e1e2a8eae6cefe1f7b387f29d9e33
Ana <yaypunk@gmail.com>
2020-02-27 21:41:38 +0100

    init commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,32 @@
+class TasksController < ApplicationController
+  def new
+    @task = Task.new
+    render :show_form
+  end
+
+  def create
+    @task = Task.create(task_params)
+    @tasks = Task.all
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    render :show_form
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    @tasks = Task.all
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(task_params)
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :note, :completed)
+  end
+end
+
